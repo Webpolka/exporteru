0a21cda6f1300ca5b8c0a133e48c1be57f80f177
Webpolka <webpolka@gmail.com>
2025-09-23 16:54:50 +0300

    fix.23

--- src/js/main.js
+++ src/js/main.js
@@ -57,22 +57,22 @@ categoriesOverlayMenu &&
 /* ------------------------------------------------------------------------------------------------------------------------------
 Main top padding listener
 --------------------------------------------------------------------------------------------------------------------------------*/
-const header = document.querySelector(".header");
+// const header = document.querySelector(".header");
 
-window.onresize = updateMainTop.bind(null, header);
-window.onchange = updateMainTop.bind(null, header);
-window.onload = updateMainTop.bind(null, header);
+// window.onresize = updateMainTop.bind(null, header);
+// window.onchange = updateMainTop.bind(null, header);
+// window.onload = updateMainTop.bind(null, header);
 
-function updateMainTop(header) {
-	if (header) {
-		const headerTopHeight = header.querySelector(".header__top").scrollHeight;
-		const headerMiddleHeight = header.querySelector(".header__medium").scrollHeight;
-		const main = document.querySelector(".main");
-		if (main) {
-			main.style.paddingTop = headerTopHeight + headerMiddleHeight + "px";
-		}
-	}
-}
+// function updateMainTop(header) {
+// 	if (header) {
+// 		const headerTopHeight = header.querySelector(".header__top").scrollHeight;
+// 		const headerMiddleHeight = header.querySelector(".header__medium").scrollHeight;
+// 		const main = document.querySelector(".main");
+// 		if (main) {
+// 			main.style.paddingTop = headerTopHeight + headerMiddleHeight - 1 + "px";
+// 		}
+// 	}
+// }
 
 /* --------------------------------------------------------------------------------------------------------------------------
 FANCYBOX
@@ -91,3 +91,58 @@ Fancybox.bind("[data-fancybox]", {
 PRODUCCT GALLERY PARALAX
 -----------------------------------------------------------------------------------------------------------------------------*/
 import "./modules/product-gallery-parallax.js";
+
+/* --------------------------------------------------------------------------------------------------------------------------
+STAR RATING
+-----------------------------------------------------------------------------------------------------------------------------*/
+import StarRating from "./modules/rating-stars.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const expirienceRating = document.querySelector(".expirience-form__rating");
+	expirienceRating &&
+		new StarRating({
+			container: ".expirience-form__rating",
+			hiddenInput: "#expirience-rating",
+		});
+});
+
+/* -------------------------------------------------------------------------------------------------------------------------------------------------
+Auto scale 
+-----------------------------------------------------------------------------------------------------------------------------------------------------*/
+document.addEventListener("DOMContentLoaded", function () {
+	function scalePage() {
+		const windowWidth = window.innerWidth;
+		const wrapper = document.querySelector(".wrapper");
+		const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+			
+
+		if (windowWidth >= 1200) {		
+			
+			const pageWidth = wrapper.offsetWidth;
+
+			const minWidth = 1200;
+			const maxWidth = 6400;
+
+			const constrainedWidth = Math.max(minWidth, Math.min(windowWidth, maxWidth));
+
+			let sum = constrainedWidth - scrollbarWidth;
+			let scaleRatio = sum / pageWidth;
+			// Пересчитываем масштаб с учетом ограничений
+
+			wrapper.style.transformOrigin = "top left";
+			wrapper.style.transform = "scale(" + scaleRatio + ")";
+			wrapper.style.overflowX = "hidden";
+		
+		} else if (windowWidth < 1200) {
+			wrapper.style.removeProperty("transform-origin");
+			wrapper.style.removeProperty("transform-scale");
+			wrapper.style.removeProperty("overflow-x");
+		
+		}
+	}
+
+	// Вызов при загрузке и при изменении размера окна
+	window.onload = scalePage();
+	window.onresize = scalePage;
+	window.onchange = scalePage;
+});
---
+++ src/js/modules/rating-stars.js
@@ -0,0 +1,63 @@
+export default class StarRating {
+	constructor(options = {}) {
+		const defaultConfig = {
+			container: ".expirience-form__rating",
+			hiddenInput: "#expirience-rating",
+		};
+		this.options = Object.assign(defaultConfig, options);
+
+		this.container = document.querySelector(this.options.container);
+		this.input = document.querySelector(this.options.hiddenInput);
+		this.starCount = 5;
+		this.stars = [];
+		this.isLocked = false;
+		this.init();
+	}
+
+	init = () => {
+		// Создаем звезды
+		const allStars = document.querySelector(this.options.container).querySelectorAll(".rating-stars__item");
+		allStars.forEach((star, i) => {
+			star.setAttribute("data-value", i + 1);
+			this.stars.push(star);
+
+			// Наведение мыши
+			star.addEventListener("mouseenter", () => {
+				if (this.isLocked) return;
+				this.highlightStars(i + 1);
+			});
+
+			// Убрать подсветку
+			star.addEventListener("mouseleave", () => {
+				if (this.isLocked) return;
+				const currentRating = parseInt(this.input.value);
+				this.highlightStars(currentRating);
+			});
+
+			// Клик по звезде
+			star.addEventListener("click", () => {
+				this.input.value = i + 1;
+				this.fillStars(i + 1);
+				this.isLocked = true;
+			});
+		}, this);
+
+		// Изначально подсветим выбранный рейтинг
+		const initialRating = parseInt(this.input.value);
+		this.fillStars(initialRating);
+	};
+
+	highlightStars(rating) {
+		this.stars.forEach((star, index) => {
+			if (index < rating) {
+				star.classList.add("filled");
+			} else {
+				star.classList.remove("filled");
+			}
+		});
+	}
+
+	fillStars(rating) {
+		this.highlightStars(rating);
+	}
+}
